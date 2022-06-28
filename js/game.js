8c04923814a1fd404ce36cc74b32efd2a7012ccd
Danilo Augusto <danilloagt@gmail.com>
2022-06-28 05:20:51 -0300

    Script load game

--- js/game.js
+++ js/game.js
@@ -1,5 +1,17 @@
 const grid = document.querySelector(".grid");
 
+const characters = [
+    "beth",
+    "jerry",
+    "jessica",
+    "meeseeks",
+    "morty",
+    "pessoa-passaro",
+    "pickle-rick",
+    "rick",
+    "scroopy",
+    "summer"
+];
 
 const createElement = (tag, className) =>{
     const element = document.createElement(tag);
@@ -8,17 +20,26 @@ const createElement = (tag, className) =>{
 }
 
 
-const createCard = () =>{
+const createCard = (character) =>{
     const card = createElement("div", "card");
     const front = createElement("div", "face front");
     const back = createElement("div", "face back");
+
+    front.style.backgroundImage = `url(../images/${character}.png)`;
     
     card.appendChild(front);
     card.appendChild(back);
 
-    grid.appendChild(card);
-
-    return grid;
+    return card;
 }
 
-createCard();
+const loadGame = () => {
+    characters.forEach((character)=>{
+        const card = createCard(character); 
+        grid.appendChild(card);
+        // console.log(character);        
+    })
+};
+
+
+loadGame();
